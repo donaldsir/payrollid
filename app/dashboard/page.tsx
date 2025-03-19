54b7d5aa6267cedd72f2aa1cf8002b9b819c5ac8
donaldpsg <donald.peaksolutions@gmail.com>
2025-03-19 17:05:35 +0800

    add description to homepage

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,22 @@
 // app/dashboard/page.tsx
+
+import { Container, Stack, Box, Heading, Text } from "@chakra-ui/react";
+
 export default function DashboardPage() {
-    return <div>Welcome to Admin Dashboard</div>
+  return (
+    <Stack spacing={8} direction="row">
+      <Box p={5} shadow="md" borderWidth="1px">
+        <Heading fontSize="xl">Simple Payroll System for Indonesia</Heading>
+        <Text mt={4}>
+          This payroll system is designed to handle basic salary processing in compliance with Indonesian tax
+          regulations. It focuses on automating salary calculations, allowances, and tax deductions to ensure accuracy
+          and efficiency in payroll management.
+        </Text>
+
+        <Text mt={4} fontSize="sm" color="gray">
+          Donald Siregar
+        </Text>
+      </Box>
+    </Stack>
+  );
 }
